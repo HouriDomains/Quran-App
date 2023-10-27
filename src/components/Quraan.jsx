f4015ef8acc2ec28f6d16791707cbbf0ce982bdc
HouriDomains <110770289+HouriDomains@users.noreply.github.com>
2023-10-27 02:01:40 -0700

    Update Quraan.jsx

--- src/components/Quraan.jsx
+++ src/components/Quraan.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Verse from "../quran.json";
+import "./components/styles.css";
 
 function Quran() {
   const [selectedSurah, setSelectedSurah] = useState("");
@@ -8,7 +9,7 @@ function Quran() {
 
   const handleSurahSelect = (e) => {
     setSelectedSurah(e.target.value);
-    setSelectedAyah(""); // Reset the selected Ayah when a new Surah is selected.
+    setSelectedAyah(""); 
   };
 
   const handleShowAllAyahs = () => {
